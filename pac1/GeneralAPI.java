ffb527f46da367c702299b998bf66a4650ca0dd0
Kacpro <kacperj97@gmail.com>
2017-12-17 12:05:32 +0100

    Added getting text from url

--- pac1/GeneralAPI.java
+++ pac1/GeneralAPI.java
@@ -1,5 +1,41 @@
 package pac1;
 
-public abstract class GeneralAPI {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public abstract class GeneralAPI 
+{
+	
+	protected String printHelp()
+	{
+		return "This API doesn't implement manual";
+	}
+	
+	
+	protected Document getXMLDoc(String strUrl) throws IOException, ParserConfigurationException, SAXException
+	{
+		URL url = new URL(strUrl);
+		BufferedReader reader = new BufferedReader(new InputStreamReader( url.openStream()));
+		String file = "";
+		String line;
+		while ((line = reader.readLine()) != null)
+		{
+			file += line;
+		}
+		reader.close();
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document doc = docBuilder.parse(file);
+		return doc;
+	}
 }
--- pac1/Main.java
+++ pac1/Main.java
@@ -2,8 +2,9 @@ package pac1;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+
 
 	}
 
